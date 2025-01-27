f72b6ef7365ea7941a1d35aefd1da35b77780b6f
YamidCampo <69608801+YamidCampo@users.noreply.github.com>
2025-01-26 21:46:13 -0500

    realizado lab 3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,7 +3,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
 # pylint: disable=import-outside-toplevel
-
+import pandas as pd
 
 def pregunta_01():
     """
@@ -18,3 +18,42 @@ def pregunta_01():
 
 
     """
+
+
+    with open('files/input/clusters_report.txt', 'r') as file:
+        lines = file.readlines()
+
+    indice = 4
+    datos_lineas = lines[indice:]
+
+    filas = []
+    fila_actual = []
+
+    for line in datos_lineas:
+        if line.strip():
+            fila_actual.append(line.strip())
+        else:
+            if fila_actual:
+                filas.append(" ".join(fila_actual))
+                fila_actual = []
+
+    datos = []
+    for fila in filas:
+        piezas = fila.split()
+        cluster = int(piezas[0])
+        cantidad = int(piezas[1])
+        porcentaje = float(piezas[2].replace(",", "."))
+        palabras_clave = (
+        " ".join(piezas[3:])
+        .replace(" ,", ",")
+        .replace(", ", ", ")
+        .strip("%")
+        .rstrip(".")
+        .strip())
+        datos.append([cluster, cantidad, porcentaje, palabras_clave])
+
+    df = pd.DataFrame(datos, columns=[
+        "cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"
+    ])
+
+    return df
